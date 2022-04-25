777e9f98961357622342d14e5689c9f9c3781815
Derek Chan <derek.chan@ztore.com>
2022-04-25 16:41:22 +0800

    add logout, remember me function

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ app = Flask(__name__,
             static_url_path='',
             static_folder='web/static/src/pages',
             template_folder='web/templates')
-
+# Forces all connects to https
+# Reference: https://github.com/GoogleCloudPlatform/flask-talisman
 Talisman(app)
 
 app.config['MYSQL_HOST'] = 'dcrhg4kh56j13bnu.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
@@ -17,10 +18,12 @@ app.config['MYSQL_DB'] = 'jxxv8laq46soz2mq'
 
 mysql = MySQL(app)
 
-
-@app.route('/')
-def hello():
-    return redirect("./login.html", code=302)
+@app.route('/') # redirecting to url in flask, ref: https://flask.palletsprojects.com/en/2.1.x/api/
+def index():
+    if 'email' in session:
+        return redirect("./student-dashboard.html", code=302)
+    else:
+        return redirect("./login.html", code=302)
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -28,8 +31,10 @@ def login():
     if request.method == 'POST':
         useremail = request.form['email']
         userpassword = request.form['password']
+        rememberme = request.form['rememberme']
         print("useremail:" + useremail)
         print("userpassword:" + userpassword)
+        print("rememberme:" + rememberme)
         cursor = mysql.connection.cursor()
         cursor.execute("SELECT * FROM User WHERE email = %s AND  pwd = %s", (useremail, userpassword))
         row_headers = [h[0] for h in cursor.description]  # extract row headers
@@ -37,6 +42,9 @@ def login():
         user = cursor.fetchone()
         if user is None:  # user not exist
             errorMsg = 'You have entered an invalid username or password!!!'
+        else:
+            if rememberme:
+                session['email'] = request.form['email']
         # Saving the Actions performed on the DB
         mysql.connection.commit()
 
@@ -48,6 +56,14 @@ def login():
         result["data"] = user
 
         return result
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it is there
+    if 'email' in session:
+        session.pop('email', None)
+
+    return redirect('/')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
