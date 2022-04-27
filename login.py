544ee91dc8c715330c7d3f6f3395eb24b8272c50
Derek Chan <derek.chan@ztore.com>
2022-04-27 14:43:36 +0800

    split api function to different python file

---
+++ login.py
@@ -0,0 +1,49 @@
+from flask import request, session, make_response, redirect
+
+from main import app, mysql
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    errorMsg = ''  # output error message if error occurred
+    result = {}
+    if request.method == 'POST':
+        useremail = request.form['email']
+        userpassword = request.form['password']
+        isRememberme = bool('rememberme' in request.form and request.form['rememberme'] == 'on')
+        print("useremail:" + useremail)
+        print("userpassword:" + userpassword)
+        cursor = mysql.connection.cursor()
+        cursor.execute("SELECT user_id, user_name, user_type, score FROM User WHERE email = %s AND  pwd = %s",
+                       (useremail, userpassword))
+        # Fetch one record and return result
+        user = cursor.fetchone()
+        if user is None:  # user not exist
+            errorMsg = 'You have entered an invalid username or password!!!'
+        else:
+            session['user'] = user
+        # Saving the Actions performed on the DB
+        mysql.connection.commit()
+
+        # Closing the cursor
+        cursor.close()
+
+        result["success"] = True if errorMsg == '' else False
+        result["errorMsg"] = errorMsg
+        result["data"] = user
+        res = make_response(result)
+        if isRememberme:
+            # cookie
+            res.set_cookie("email", value=str(useremail))
+        else:
+            res.set_cookie("email", value="", expires=0)
+        return res
+
+
+@app.route('/logout')
+def logout():
+    # remove the user from the session if it is there
+    if 'user' in session:
+        session.pop('user', None)
+
+    return redirect('/')
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, request, Response, session, make_response, render_template
+from flask import Flask
 from flask_mysqldb import MySQL
 import os
 
@@ -16,58 +16,17 @@ app.config['MYSQL_DB'] = 'jxxv8laq46soz2mq'
 app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 mysql = MySQL(app)
 
-@app.route('/', methods=['POST', 'GET'])  # redirecting to url in flask, ref: https://flask.palletsprojects.com/en/2.1.x/api/
+
+@app.route('/', methods=['POST', 'GET'])
+# redirecting to url in flask, ref: https://flask.palletsprojects.com/en/2.1.x/api/
 def index():
     if 'user' in session:
         return redirect("./student-dashboard.html", code=302)
     else:
         return redirect("./login.html", code=302)
 
-
-@app.route('/login', methods=['POST'])
-def login():
-    errorMsg = ''  # output error message if error occurred
-    result = {}
-    if request.method == 'POST':
-        useremail = request.form['email']
-        userpassword = request.form['password']
-        isRememberme = bool('rememberme' in request.form and request.form['rememberme'] == 'on')
-        print("useremail:" + useremail)
-        print("userpassword:" + userpassword)
-        cursor = mysql.connection.cursor()
-        cursor.execute("SELECT user_id, user_name, user_type, score FROM User WHERE email = %s AND  pwd = %s", (useremail, userpassword))
-        # Fetch one record and return result
-        user = cursor.fetchone()
-        if user is None:  # user not exist
-            errorMsg = 'You have entered an invalid username or password!!!'
-        else:
-            session['user'] = user
-        # Saving the Actions performed on the DB
-        mysql.connection.commit()
-
-        # Closing the cursor
-        cursor.close()
-
-        result["success"] = True if errorMsg == '' else False
-        result["errorMsg"] = errorMsg
-        result["data"] = user
-        res = make_response(result)
-        if isRememberme:
-            #cookie
-            res.set_cookie("email", value=str(useremail))
-        else:
-            res.set_cookie("email", value="", expires=0)
-        return res
-
-
-@app.route('/logout')
-def logout():
-    # remove the user from the session if it is there
-    if 'user' in session:
-        session.pop('user', None)
-
-    return redirect('/')
-
+# import login api
+from login import *
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
