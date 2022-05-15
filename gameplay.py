4ae7306a0cb319a57c71f46db51fcc116e717be7
Derek Chan <derek.chan@ztore.com>
2022-05-16 00:04:22 +0800

    update gameplay page logic

--- gameplay.py
+++ gameplay.py
@@ -2,6 +2,39 @@ from flask import request, session, make_response, redirect
 
 from main import app, mysql
 
+
+# @app.route('/createQuizRecord', methods=['POST','GET'])
+# def createQuizRecord():
+#     errorMsg = ''  # output error message if error occurred
+#     result = {}
+#     if request.method == 'POST':
+#         args = request.args
+#         quizId = args['quizid']
+#         user = session['user']
+#         studentId = user['user_id']
+#         cursor = mysql.connection.cursor()
+#         cursor.execute("SELECT user_quiz_id, UserQuiz.user_id, UserQuiz.quiz_id "
+#                        "from UserQuiz, User "
+#                        "where UserQuiz.user_id = User.user_id "
+#                        "and user_id = %s and UserQuiz.quiz_id = %s", (str(studentId), str(quizId)))
+#         # Fetch records and return result
+#         record = cursor.fetchall()
+#         if len(record) == 0:  # user does not have record
+#             errorMsg = 'record creating...'
+#             cursor.execute("INSERT INTO UserQuiz (user_id, quiz_id, current_progress, score, status) VALUES (%s. %s, 0, 0, 'ready')",(str(studentId), str(quizId)))
+#         # Saving the Actions performed on the DB
+#         mysql.connection.commit()
+#         # Closing the cursor
+#         cursor.close()
+#
+#         result["success"] = True if errorMsg == '' else False
+#         result["errorMsg"] = errorMsg
+#         result["data"] = record
+#         res = make_response(result)
+#
+#         return res
+
+
 @app.route('/getCurrentQuizTitle', methods=['POST','GET'])
 def getCurrentQuizTitle():
     errorMsg = ''  # output error message if error occurred
@@ -12,14 +45,14 @@ def getCurrentQuizTitle():
         user = session['user']
         studentId = user['user_id']
         cursor = mysql.connection.cursor()
-        cursor.execute("SELECT UserQuiz.quiz_id,quiz_name, quiz_status, quiz_score, current_progress, score, status, subject_name "
+        cursor.execute("SELECT UserQuiz.quiz_id, quiz_name, quiz_status, quiz_score, current_progress, score, status, subject_name "
                        "from UserQuiz, Quiz, Subject "
                        "where UserQuiz.quiz_id = Quiz.quiz_id "
                        "and Quiz.subject_id = Subject.subject_id "
                        "and user_id = %s and UserQuiz.quiz_id = %s", (str(studentId), str(quizId)))
         # Fetch records and return result
         quizList = cursor.fetchall()
-        if len(quizList) == 0:  # user does not have quiz
+        if len(quizList) == 0:  # cannot find quiz
             errorMsg = 'quizList is empty'
         # Saving the Actions performed on the DB
         mysql.connection.commit()
@@ -29,6 +62,75 @@ def getCurrentQuizTitle():
         result["success"] = True if errorMsg == '' else False
         result["errorMsg"] = errorMsg
         result["data"] = quizList
+        res = make_response(result)
+
+        return res
+
+@app.route('/getQuestionList', methods=['POST','GET'])
+def getQuestionList():
+    errorMsg = ''  # output error message if error occurred
+    result = {}
+    if request.method == 'GET':
+        args = request.args
+        quizId = args['quizid']
+        user = session['user']
+        studentId = user['user_id']
+        cursor = mysql.connection.cursor()
+        cursor.execute("SELECT  user_quiz_question_id, UserQuizQuestion.user_quiz_id , user_id, UserQuizQuestion.question_id, question, option_a, option_b, option_c, option_d, correct_answer, quiz_score "
+                       "FROM UserQuiz, UserQuizQuestion, Question, Quiz "
+                       "WHERE UserQuiz.user_quiz_id = UserQuizQuestion.user_quiz_id "
+                       "AND UserQuizQuestion.question_id = Question.question_id "
+                       "AND UserQuiz.quiz_id = Quiz.quiz_id "
+                       "AND user_id = %s AND UserQuiz.quiz_id = %s;", (str(studentId), str(quizId)))
+        # Fetch records and return result
+        questionsList = cursor.fetchall()
+        if len(questionsList) == 0:  # quiz don't have any question
+            errorMsg = 'questionsList is empty'
+        # Saving the Actions performed on the DB
+        mysql.connection.commit()
+        # Closing the cursor
+        cursor.close()
+
+        result["success"] = True if errorMsg == '' else False
+        result["errorMsg"] = errorMsg
+        result["data"] = questionsList
+        res = make_response(result)
+
+        return res
+
+@app.route('/updateUserQuizStatus', methods=['POST','GET'])
+def updateUserQuizStatus():
+    errorMsg = ''  # output error message if error occurred
+    result = {}
+    if request.method == 'GET':
+        args = request.args
+        print(args)
+        uqqid = args['uqqid']
+        uqid = args['uqid']
+        questionid = args['questionid']
+        answer = args['answer']
+        isCorrect = args['isCorrect']
+        currentprogress = args['currentprogress']
+        totalmark = args['totalmark']
+        currentStatus = args['currentStatus']
+
+        cursor = mysql.connection.cursor()
+        cursor.execute("UPDATE UserQuizQuestion "
+                       "SET user_answer =  %s, is_correct = %s WHERE user_quiz_question_id = %s AND question_id = %s;",
+                       (str(answer), isCorrect, uqqid, questionid))
+
+        cursor.execute("UPDATE UserQuiz "
+                       "SET current_progress =  %s, score = %s, status = %s WHERE user_quiz_id = %s;",
+                       (currentprogress, totalmark, currentStatus, uqid))
+
+        # Saving the Actions performed on the DB
+        mysql.connection.commit()
+        # Closing the cursor
+        cursor.close()
+
+        result["success"] = True if errorMsg == '' else False
+        result["errorMsg"] = errorMsg
+        result["data"] = ""
         res = make_response(result)
 
         return res
